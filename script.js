2eebeb1a0e44dccbb6a98823d98ec9ab0735a725
konradkn <konradkn@outlook.com>
2023-04-12 10:20:19 -0600

    add display icons functionality, winner animation and css enhancement

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ let possibleChoices = document.querySelectorAll("button");
 let resetButtonSelector = document.getElementById("reset-button");
 let scoreSelectorPlayer = document.getElementById("score");
 let scoreSelectorComputer = document.getElementById("score-computer");
-let roundWinnerSelector = document.getElementById('round-winner')
+let roundWinnerSelector = document.getElementById("round-winner");
+let imagesContainerSelector = document.getElementById("icons-container");
+let imagesContainerUserSelector = document.getElementById(
+  "icons-container-user"
+);
 
+let mainGameSelector = document.getElementById("main-game");
 ////////////////////////////////////////
-let imagesContainerSelector = document.getElementById('icons-container')
 
 let userChoice;
 let computerChoice;
@@ -21,11 +25,12 @@ let resetButton = () => {
   document.location.reload();
 };
 
-let disabledButtons = []
+let disabledButtons = [];
 
 possibleChoices.forEach((choice) =>
   choice.addEventListener("click", (e) => {
     userChoice = e.target.id;
+    console.log(userChoice);
     userSelector.innerHTML = userChoice;
     e.target.disabled = true;
     computerChoiceGenerator();
@@ -33,52 +38,110 @@ possibleChoices.forEach((choice) =>
     updateScore();
 
     //check if all buttons are disabled to give the the final result in each round
-    disabledButtons.push(e.target.disabled)
-    console.log(disabledButtons)
-    if(disabledButtons.length === 5) {
-      roundWinner()
+    disabledButtons.push(e.target.disabled);
+    console.log(disabledButtons);
+    if (disabledButtons.length === 5) {
+      roundWinner();
+      loadingWinner();
     }
 
+    //create icon elements with each user choice
+    let userIcon = document.createElement("div");
+
+    if (userChoice == "Rock") {
+      userIcon.classList.add("rock-icon");
+      imagesContainerUserSelector.append(userIcon);
+    } else if (userChoice == "Paper") {
+      userIcon.classList.add("paper-icon");
+      imagesContainerUserSelector.append(userIcon);
+    } else if (userChoice == "Scissors") {
+      userIcon.classList.add("scissors-icon");
+      imagesContainerUserSelector.append(userIcon);
+    } else if (userChoice == "Lizard") {
+      userIcon.classList.add("lizard-icon");
+      imagesContainerUserSelector.append(userIcon);
+    } else {
+      userIcon.classList.add("spock-icon");
+      imagesContainerUserSelector.append(userIcon);
+    }
   })
 );
 
+//////////////////////////
 
-const roundWinner = () => {
-  if(counter < counterComputer) {
-    roundResult = 'computer won!' 
-  } else if(counter > counterComputer){
-    roundResult = 'you won!'
-  } else {
-    roundResult = 'nobody won!'
-  }
+const opacityChange = [
+  { opacity: 1 },
+  { backgroundColor: "#525252" },
+  { opacity: 0.5 },
+];
 
-  roundWinnerSelector.innerHTML = roundResult;
-  // roundResult.style.color = "#19ff66"
-  if(roundResult.includes('you')) {
-    roundWinnerSelector.style.color = "#19ff66"
-  } else if(roundResult.includes('computer')) {
-    roundWinnerSelector.style.color = "#ff3f3f"
-  } else if(roundResult.includes('nobody')){
-    roundWinnerSelector.style.color = "#ffffff"
-  }
+const opacityTiming = {
+  duration: 300,
+  iterations: 4,
 };
 
+const loadingWinner = () => {
+  // mainGameSelector.style.display = 'none'
+  mainGameSelector.animate(opacityChange, opacityTiming);
+};
+
+const addIcons = () => {};
+
+///////////////////////
+
+const roundWinner = () => {
+  setTimeout(() => {
+    if (counter < counterComputer) {
+      roundResult = "computer won this round!";
+    } else if (counter > counterComputer) {
+      roundResult = "you won this round!";
+    } else {
+      roundResult = "nobody won this round!";
+    }
+
+    roundWinnerSelector.innerHTML = roundResult;
+    // roundResult.style.color = "#19ff66"
+    if (roundResult.includes("you")) {
+      roundWinnerSelector.style.color = "#19ff66";
+    } else if (roundResult.includes("computer")) {
+      roundWinnerSelector.style.color = "#ff3f3f";
+    } else if (roundResult.includes("nobody")) {
+      roundWinnerSelector.style.color = "#ffffff";
+    }
+  }, 1700);
+};
 
 resetButtonSelector.addEventListener("click", resetButton);
 
 let computerChoiceGenerator = () => {
   const randomNum = Math.floor(Math.random() * possibleChoices.length) + 1;
+  let iconCreation = document.createElement("div");
 
   if (randomNum === 1) {
     computerChoice = "Rock";
+
+    iconCreation.classList.add("rock-icon");
+    imagesContainerSelector.append(iconCreation);
   } else if (randomNum === 2) {
     computerChoice = "Paper";
+
+    iconCreation.classList.add("paper-icon");
+    imagesContainerSelector.append(iconCreation);
   } else if (randomNum === 3) {
     computerChoice = "Scissors";
+
+    iconCreation.classList.add("scissors-icon");
+    imagesContainerSelector.append(iconCreation);
   } else if (randomNum === 4) {
     computerChoice = "Lizard";
+
+    iconCreation.classList.add("lizard-icon");
+    imagesContainerSelector.append(iconCreation);
   } else {
     computerChoice = "Spock";
+
+    iconCreation.classList.add("spock-icon");
+    imagesContainerSelector.append(iconCreation);
   }
 
   computerSelector.innerHTML = computerChoice;
@@ -132,7 +195,7 @@ let getResult = () => {
     counterComputer++;
   } else if (computerChoice == "Lizard" && userChoice == "Scissors") {
     matchResult = "Scissors decapitate lizard! You win!";
-    counter++
+    counter++;
   } else if (computerChoice == "Lizard" && userChoice == "Rock") {
     matchResult = "Rock crushes lizard! You win";
     counter++;
